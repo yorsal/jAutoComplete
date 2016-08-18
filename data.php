162ad718b22e06df7db69f02cb1dd303cbb2ebae
yorsal <leyang@soocook.com>
2016-08-18 17:06:02 +0800

    testing data

--- data.php
+++ data.php
@@ -1,7 +1,7 @@
 <?php 
 $query = $_GET['query'];
 $data = array();
-$data[] = array('value'=>$query.'1111');
-$data[] = array('value'=>$query.'2222');
+$data[] = array('value'=>$query.'hello world');
+$data[] = array('value'=>$query.'hello bady');
 echo json_encode($data);
 ?>
